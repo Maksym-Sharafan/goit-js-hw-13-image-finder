ddf8cef154d0a9d0af5f1bc8c36d65de3a97553e
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-06-02 12:33:06 +0300

    add infinity scroll instead button

--- src/index.js
+++ src/index.js
@@ -11,31 +11,32 @@ import objectFetchAPI from './js/apiService.js';
 
 const formForSearch = document.getElementById('search-form');
 const imagesList = document.querySelector('.gallery');
-const buttonLoadMore = document.querySelector('.load_more');
+// const buttonLoadMore = document.querySelector('.load_more');
 const trigger = document.querySelector('.trigger');
 
 
 formForSearch.addEventListener('input', debounce(takeFoundItems, 500));
-buttonLoadMore.addEventListener('click', toLoadMore);
+// buttonLoadMore.addEventListener('click', toLoadMore);
 imagesList.addEventListener('click', handleModalImg);
 
 function takeFoundItems(e) {
     e.preventDefault();
 
     clearContainer();
+    
     objectFetchAPI.query = e.target.value.trim();
-
     if (!objectFetchAPI.query) return;
 
     handleTakeData();
-    buttonLoadMore.classList.remove('invisible_btn');
+    // buttonLoadMore.classList.remove('invisible_btn');
 };
 
 async function handleTakeData() {
+
     try {
         const response = await objectFetchAPI.onFetchAPI();
-        renderImageList(response.hits);
-        return response.hits;
+        renderImageList(response);
+        console.log(response);
     } catch (err) {
         showFetchError(err);
     }
@@ -51,15 +52,15 @@ function clearContainer() {
     if (objectFetchAPI.page > 1) {
         return objectFetchAPI.page = 1;
     }
-    buttonLoadMore.classList.add('invisible_btn');
+    // buttonLoadMore.classList.add('invisible_btn');
 };
 
-function toLoadMore() {
-    objectFetchAPI.page += 1;
-    handleTakeData();
-    imagesList.scrollIntoView({ block: "end", behavior: "smooth" });
-    return;
-};
+// function toLoadMore() {
+//     // objectFetchAPI.page += 1;
+//     handleTakeData();
+//     buttonLoadMore.scrollIntoView({ block: "end", behavior: "smooth" });
+//     return;
+// };
 
 function handleModalImg(e) {
     e.preventDefault();
@@ -89,26 +90,17 @@ function showFetchError(err) {
 };
 
 
-// document.addEventListener("scroll", intersectionObserver);
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && objectFetchAPI.query !== '') {
+      objectFetchAPI.onFetchAPI().then(articles => {
+        renderImageList(articles);
+      });
+    }
+  });
+};
 
-// function intersectionObserver() {
-//     let options = {
-//         root: null,
-//         rootMargins: "0px",
-//         threshold: 1.0,
-//     };
-//     const observer = new IntersectionObserver(handleIntersect, options);
-//     observer.trigger;
-//     //an initial load of some data
-//     handleTakeData();
-
-//     document.removeEventListener("scroll", intersectionObserver);
-// }
-
-
-// async function handleIntersect() {
-//     console.warn("something is intersecting with the viewport");
-//     objectFetchAPI.page += 1;
-//     handleTakeData();
-
-// };
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '150px',
+});
+observer.observe(trigger);
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,22 +3,28 @@ const objectAPI = {
     BASE_URL : 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&',
     MY_API_KYE: '21844309-0bdc8d8d935c894ec75615f09',
     query: '',
-    page:1,
+    page: 1,
+    incrementPage() {
+        this.page += 1;
+    },
 
-    onFetchAPI() {
-        if (this.query === '') return;
-    return fetch(`${this.BASE_URL}q=${this.query}&page=${this.page}&per_page=12&key=${this.MY_API_KYE}`)
-        .then(response => { return response.json() })
+     onFetchAPI() {
+         if (this.query === '') return;
+         
+        return fetch(`${this.BASE_URL}q=${this.query}&page=${this.page}&per_page=12&key=${this.MY_API_KYE}`)
+            .then(response => { return response.json() })
+            .then(({ hits })=>{
+                this.incrementPage();
+                return hits;
+            })
+            .catch(err=>{console.log(err)});
     },
 };
 export default objectAPI;
 
 
-// const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
-// const MY_API_KYE = '21844309-0bdc8d8d935c894ec75615f09';
-      
-//  function onFetchAPI(search, page) {
-//     // let page = numberPage;
-//     return fetch(`${BASE_URL}q=${search}&page=${page}&per_page=12&key=${MY_API_KYE}`)
+// onFetchAPI() {
+//         if (this.query === '') return;
+//     return fetch(`${this.BASE_URL}q=${this.query}&page=${this.page}&per_page=12&key=${this.MY_API_KYE}`)
 //         .then(response => { return response.json() })
-// };
+//     },
